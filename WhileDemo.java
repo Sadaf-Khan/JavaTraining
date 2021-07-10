31fc0342080ec01dd6d6ca32990a8b3c875ee144
Sadaf-Khan <86579117+Sadaf-Khan@users.noreply.github.com>
2021-07-10 13:21:36 +0530

    Add files via upload

---
+++ WhileDemo.java
@@ -0,0 +1,19 @@
+package inspi.java;
+import java.util.Scanner;
+public class WhileDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number, sum = 0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("n Please Enter any integer Value below 10: ");
+		number = sc.nextInt();
+		while (number <= 10) 
+		{
+		sum = sum + number;
+		number++;
+		}
+		System.out.format(" Sum of the Numbers From the While Loop is: %d ",sum);
+	}
+
+}
